6cc7b1410ce67a7e4b4afd8cbacb188ca87e6d70
5656565566 <contact@home56.top>
2026-08-18 17:18:39 +0800

    新增：提供通用 MSA OAID 兼容服务

--- nativebridge.mk
+++ nativebridge.mk
@@ -1,7 +1,8 @@
 # Install the aggregate entry point used by ART's NativeBridge loader.
 PRODUCT_PACKAGES += \
     floral_nativebridge_runner \
-    libmixbridge
+    libmixbridge \
+    floral_msa_compat
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.dalvik.vm.native.bridge=libmixbridge.so \
